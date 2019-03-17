9882be7be426b4e06957d2412445fb440a6cc2eb
Sergey Chirva <19sem92@gmail.com>
2019-03-17 23:03:35 +0200

    vuemastery Real World Vue.js

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Start json-server
+```
+json-server --watch {file} // db.json
+```
---
+++ src/views/EventList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <event-card
+      v-for="(event, i) in events"
+      :key="event.id"
+      :event="event"
+      :index="i + 1"
+    />
+    <base-icon/>
+  </div>
+</template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+// import axios from 'axios'
+import EventService from "@/services/EventService.js";
+export default {
+  name: "EventList",
+  components: {
+    EventCard
+  },
+  data() {
+    return {
+      events: []
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then(res => {
+        this.events = res.data;
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    console.log("already created");
+  },
+  mounted() {
+    console.log("already mounted");
+  }
+};
+</script>
