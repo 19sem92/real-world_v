7f98511bc7a406ad26cde1f16059dadf988cbe41
Sergey Chirva <sergey.chirva@adraba.local>
2019-03-18 14:44:19 +0200

    add eslint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,10 +5,22 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "linebreak-style": 0,
+    "new-cap": 0,
+    "semi": ["error", "never"],
+    "no-console": "off",
+    "no-debugger": "off",
+    "import/no-unresolved": 0,
+    "import/no-unassigned-import": 0,
+    "standard/object-curly-even-spacing": 0,
+    "space-before-function-paren": [
+      "error",
+      {
+        "anonymous": "always",
+        "named": "always",
+        "asyncArrow": "always"
+      }
+    ]
   },
-  parserOptions: {
-    parser: "babel-eslint"
-  }
-};
+  globals: {}
+}
